b2b0db918d80e87cf5f3d44945589fcb1a994657
swat1508 <swatantrasinha1508@gmail.com>
2024-06-02 19:50:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       Hello , yes its working!!
+      </div>
+    );
+  }
+}
+
+export default App;
